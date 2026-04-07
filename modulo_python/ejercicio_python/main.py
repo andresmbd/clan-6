28d9953b1b8afda793bf51c7f0ca5a867bc5675d
Andres Barrios <anmabadi@outlook.com>
2026-04-07 15:42:32 -0500

    html

---
+++ modulo_python/ejercicio_python/main.py
@@ -0,0 +1,56 @@
+# import datetime
+# rightnow = datetime.datetime.now()
+# day_of_week = rightnow.weekday()
+# hour_day = rightnow.hour
+
+# weekend = day_of_week > 5
+
+# print(f"Hey! It's {hour_day}:00")
+
+# if weekend:
+#     if hour_day < 10:
+#         print("go to sleep!! Its Weekend!!!")
+#     elif 10 <= hour_day <= 17:
+#         print("I recommend you to walk, wacth movies/serires,")
+
+#     else:
+#         print("go rest, you had a productive day")
+
+# else:
+#     if hour_day < 8:
+#         print("wake up, have your breakfast,do exersice so you get energy!!")
+#     elif 8 <= hour_day <= 17:
+#         print("Study/do your job")
+#     else:
+#         print("Go rest tomorrow is another day!")
+
+
+
+
+
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+# usuario = "riwicoder"
+# contraseña = "Qwe.123*"
+# cod_verificacion = "123456"
+
+# print("ACCESO DE SEGURIDAD\n")
+
+# usuario_ingresado = input("Ingrese su usuario: ").lower()
+# contraseña_ingresada = input("Ingrese la contraseña: ")
+
+# if usuario == usuario_ingresado and contraseña == contraseña_ingresada:
+#     print("\nPor terminos de seguridad requerimos el codigo de seguridad\n")
+#     cod_verificacion_ingresado = input("Ingrese el codigo de verificacion: ")
+
+#     if cod_verificacion == cod_verificacion_ingresado:
+#         print("\nACCESO CONCEDIDO, Bienvenido\n")
+#     else:
+#         print("\nACCESO DENEGADO, Intentelo más tarde\n")
+# else:
+#     print("\nEl usuario y/o la contraseña es incorrecto\n")
