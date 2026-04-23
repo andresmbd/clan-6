1e657e5e87a229b00791eaec2f65a43c54f6d220
Andres Barrios <anmabadi@outlook.com>
2026-04-23 15:06:52 -0500

    landing page

--- portafolio_jander/script.js
+++ portafolio_jander/script.js
@@ -1,11 +1,10 @@
 let fondo = document.querySelector(".fondo");
 let animacion = false;
-let fondoBajo = document.querySelector(".fondo-bajo");
+// let fondoBajo = document.querySelector(".fondo-bajo");
 
 function updateFondo() {
   let moveY = window.scrollY;
   let maxScroll = 400;
-                            //
   let movimiento = Math.min(moveY / maxScroll, 1); //  Math.min()Esta función elige el número más pequeño entre el resultado y 1. Sirve para que, aunque bajes 1000px, el valor de movimiento nunca sea mayor a 1 (100%). 
   //moveY / maxScroll Divide lo que has bajado entre el máximo (400px). Esto crea un porcentaje de 0 a 1.
   let escala = 1 + movimiento * 5;
@@ -18,24 +17,24 @@ function updateFondo() {
 window.addEventListener("scroll", () => {
   if (!animacion) {
     animacion = true;
-    requestAnimationFrame(updatefondo);
+    requestAnimationFrame(updateFondo);
   }
 });
 
-function updatefondobajo() {
-  let moveY = window.scrollY;
-  let maxScroll = 400;
-  let movimiento = Math.max(moveY / maxScroll, 1);
-  let escala = 1 + movimiento * 5;
-  let opacidad = 1 - movimiento;
+// function updatefondobajo() {
+//   let moveY = window.scrollY;
+//   let maxScroll = 400;
+//   let movimiento = Math.max(moveY / maxScroll, 1);
+//   let escala = 1 + movimiento * 5;
+//   let opacidad = 1 - movimiento;
 
-  fondoBajo.style.transform = `scale(${escala})`;
-  fondoBajo.style.opacity = opacidad;
-  animacion = false;
-}
-window.addEventListener("scroll", () => {
-  if (!animacion) {
-    animacion = true;
-    requestAnimationFrame(updatefondobajo);
-  }
-});
+//   fondoBajo.style.transform = `scale(${escala})`;
+//   fondoBajo.style.opacity = opacidad;
+//   animacion = false;
+// }
+// window.addEventListener("scroll", () => {
+//   if (!animacion) {
+//     animacion = true;
+//     requestAnimationFrame(updatefondobajo);
+//   }
+// });
