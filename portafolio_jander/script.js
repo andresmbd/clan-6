56fa26eb0c5fe195e0f781fbcc6903ace974b8c2
Andres Barrios <anmabadi@outlook.com>
2026-04-23 08:55:35 -0500

    javascript

--- portafolio_jander/script.js
+++ portafolio_jander/script.js
@@ -1,40 +1,41 @@
-let fondo = document.querySelector('.fondo');
+let fondo = document.querySelector(".fondo");
 let animacion = false;
-let fondoBajo= document.querySelector('.fondo-bajo')
+let fondoBajo = document.querySelector(".fondo-bajo");
 
-function updatefondo(){
-    let moveY = window.scrollY;
-    let maxScroll = 400;
-    let movimiento = Math.min(moveY / maxScroll,1);
-    let escala = 1 + movimiento * 5;
-    let opacidad = 1 - movimiento;
+function updateFondo() {
+  let moveY = window.scrollY;
+  let maxScroll = 400;
+                            //
+  let movimiento = Math.min(moveY / maxScroll, 1); //  Math.min()Esta función elige el número más pequeño entre el resultado y 1. Sirve para que, aunque bajes 1000px, el valor de movimiento nunca sea mayor a 1 (100%). 
+  //moveY / maxScroll Divide lo que has bajado entre el máximo (400px). Esto crea un porcentaje de 0 a 1.
+  let escala = 1 + movimiento * 5;
+  let opacidad = 1 - movimiento;
 
-    fondo.style.transform = `scale(${escala})`;
-    fondo.style.opacity = opacidad;
-    animacion = false;
+  fondo.style.transform = `scale(${escala})`;
+  fondo.style.opacity = opacidad;
+  animacion = false;
 }
-window.addEventListener('scroll', ()=>{
-
-    if(!animacion){
-        animacion = true;
-        requestAnimationFrame(updatefondo);
-    }
-})
+window.addEventListener("scroll", () => {
+  if (!animacion) {
+    animacion = true;
+    requestAnimationFrame(updatefondo);
+  }
+});
 
 function updatefondobajo() {
-    let moveY = window.scrollY;
-    let maxScroll = 400;
-    let movimiento = Math.max(moveY / maxScroll,1);
-    let escala = 1 + movimiento * 5;
-    let opacidad = 1 - movimiento;
+  let moveY = window.scrollY;
+  let maxScroll = 400;
+  let movimiento = Math.max(moveY / maxScroll, 1);
+  let escala = 1 + movimiento * 5;
+  let opacidad = 1 - movimiento;
 
-    fondoBajo.style.transform = `scale(${escala})`;
-    fondoBajo.style.opacity = opacidad;
-    animacion = false;
+  fondoBajo.style.transform = `scale(${escala})`;
+  fondoBajo.style.opacity = opacidad;
+  animacion = false;
 }
-window.addEventListener('scroll', ()=>{
-    if(!animacion){
-        animacion = true;
-        requestAnimationFrame(updatefondobajo);
-    }
-})
+window.addEventListener("scroll", () => {
+  if (!animacion) {
+    animacion = true;
+    requestAnimationFrame(updatefondobajo);
+  }
+});
