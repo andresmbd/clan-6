51f196cbf174ed58d32d1edd4e8406f336d422b4
Andres Barrios <anmabadi@outlook.com>
2026-04-26 22:04:52 -0500

    landing page

---
+++ landing_page/script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('.btn-menu');
+const menu = document.querySelector('.ul-nav');
+button.addEventListener('click', ()=>{
+    menu.classList.toggle('show');
+});
+
+const buttons = document.querySelectorAll('.btn')
+buttons.forEach((button) => {
+    button.addEventListener('click', ()=>{
+        alert('Your ticket has been reserved')
+    })
+})
+// button.addEventListener('click',)
+    // alert('Your ticket has been reserved'
